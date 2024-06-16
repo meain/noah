c1ccd1fb84e44588c3044d78871d84f7f8390d2c
Abin Simon <mail@meain.io>
2024-06-16 11:23:58 +0530

    Trip leading and trailing whilespace in title

--- utils.go
+++ utils.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"net/url"
+	"strings"
 )
 
 func maybeURLDecode(s string) string {
+	s = strings.TrimSpace(s)
 	decoded, err := url.QueryUnescape(s)
 	if err != nil {
 		return s
